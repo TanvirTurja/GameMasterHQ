be1146630e35a00b6a7e4ee1c9041bd340cbd0c3
Tanvir Turja <tanvirturja@yahoo.com>
2023-12-26 14:52:20 +0000

    2nd commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,35 @@
       <h1 class="sm:text-3xl text-2xl font-medium title-font mx-auto leading-relaxed xl:mb-4 text-gray-900 dark:text-gray-300">GameMasterHQ </h1>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-gray-900 dark:text-gray-300">djdkjfhdkjhd </p>
     </div>
+    <form @submit.prevent="store">
+    <div class="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+
+      <div class="relative flex-grow w-full">
+        <input type="text" name="text" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" v-model="text" placeholder="Enter ....">
+      </div>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Summarize</button>
+    </div>
+      </form>
      
+  </div>
+</section>
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto" v-show="this.summary != 0">
+    <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
+        <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+      </svg>
+      <p class="leading-relaxed text-lg">{{summary}}</p>
+      <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
+      
+    </div>
+  </div>
+  <div v-show=" this.showloading == true ">
+    <div id="loading" class="loading-container">
+    <div class="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
+  </div>
+
   </div>
 </section>
 
@@ -22,13 +50,17 @@
 </template>
 
 <script>
-// import axios from "axios";
+ import axios from "axios";
 export default{
   components: {
   
   },
   data(){
     return {
+       text :'',
+       apiKey : import.meta.env.VITE_API_KEY,
+        summary : '',
+        showloading : false
      
     }
   },
@@ -40,6 +72,39 @@ export default{
    
   },
   methods: {
+     store(){
+        this.getSummary()
+      },
+       async getSummary(){
+        
+        this.showloading = true
+
+        try {
+          
+          const response = await axios.get('https://rawg-video-games-database.p.rapidapi.com/games/', {
+          params: {
+            game_pk: this.text
+          },
+          headers: {
+           'X-RapidAPI-Key': this.apiKey,
+           'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+          }
+        })
+
+        console.log(response)
+        this.summary = response.data.summary
+        this.showloading = false
+          
+        } catch (error) {
+          console.error(error)
+          
+        }
+       
+
+
+      }
+      
+
     
    
   }
