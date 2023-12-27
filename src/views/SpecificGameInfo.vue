5168f4ee34e259ebfe4ac9c28e70a90d19b70ff7
Tanvir Turja <tanvirturja@yahoo.com>
2023-12-27 20:18:27 +0000

    test

--- src/views/SpecificGameInfo.vue
+++ src/views/SpecificGameInfo.vue
@@ -1,14 +1,14 @@
 <template>
-    <section>
-         <div class="container px-5 py-24 mx-auto" >
-    <div class=" w-full mx-auto text-center">
-      <section class="text-gray-600 body-font overflow-hidden">
+   
+      <p class="leading-relaxed text-lg">{{this.summary.name}}</p>
+     <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
+     <section class="text-gray-600 body-font overflow-hidden">
   <div class="container px-5 py-24 mx-auto">
     <div class="lg:w-4/5 mx-auto flex flex-wrap">
-      <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="{{this.summary.background_image}}">
+      <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400">
       <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 class="text-sm title-font text-gray-500 tracking-widest">{{this.summary.metacritic_platforms}}</h2>
-        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{this.summary.name}}</h1>
+        <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
+        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
         <div class="flex mb-4">
           <span class="flex items-center">
             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
@@ -26,7 +26,7 @@
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
             </svg>
-            <span class="text-gray-600 ml-3">{{this.summary.rating}}</span>
+            <span class="text-gray-600 ml-3">4 Reviews</span>
           </span>
           <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
             <a class="text-gray-500">
@@ -46,7 +46,7 @@
             </a>
           </span>
         </div>
-        <p class="leading-relaxed">{{this.summary.description}}</p>
+        <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
         <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
           <div class="flex">
             <span class="mr-3">Color</span>
@@ -84,19 +84,6 @@
     </div>
   </div>
 </section>
-
-  
-      
-     
-     
-
-      <!-- <p class="leading-relaxed text-lg">{{this.summary.name}}</p>
-     <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span> -->
-     
-      
-    </div>
-  </div>
-    </section>
 </template>
 
 <script>
