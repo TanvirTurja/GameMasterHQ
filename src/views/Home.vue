b8f3783157fab43318ed8d26edea3656492e4eb4
Tanvir Turja <tanvirturja@yahoo.com>
2023-12-27 21:08:27 +0000

    changes in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -43,20 +43,20 @@
 
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap w-full mb-20">
+  
       <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
-        <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">New Popular Games </h1>
+       
       </div>
-      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
-    </div>
-    <div class="flex flex-wrap -m-4">
-      <div class="xl:w-1/4 md:w-1/2 p-4" v-for="i in 4 " :key="i">
+    <div class="flex flex-wrap ">
+      <div class="xl:w-1/4 md:w-1/2 p-4" v-for="i in this.results " :key="i">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
-          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="{{i.background_image}}" alt="content">
+          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">{{i.released}}</h3>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{{i.name}}</h2>
+          <p class="leading-relaxed text-base" v-for="x in i.genres" :key="x" >{{x.name}}</p>
+          <p class="leading-relaxed text-base">{{i.metacritic}}</p>
+          
         </div>
       </div>
       
@@ -82,7 +82,8 @@ export default{
     return {
        text :'',
         summary : '',
-        showloading : false
+        showloading : false,
+        results : []
      
     }
   },
@@ -137,7 +138,9 @@ export default{
           page_size: 10}
         })
 
-        // console.log(response)
+        this.results = response.data.results
+
+        console.log(this.results)
        
         } catch (error) {
           console.error(error)
--- src/views/SpecificGameInfo.vue
+++ src/views/SpecificGameInfo.vue
@@ -1,7 +1,7 @@
 <template>
    
-      <!-- <p class="leading-relaxed text-lg">{{this.summary.name}}</p>
-     <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span> -->
+      <!-- <p class="leading-relaxed text-lg">{{this.summary.name}}</p> -->
+     <!-- <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span> -->
      <section class="text-gray-600 body-font overflow-hidden">
   <div class="container px-5 py-24 mx-auto">
     <div class="lg:w-4/5 mx-auto flex flex-wrap">
@@ -10,23 +10,12 @@
         <h2 class="text-sm title-font text-gray-500 tracking-widest">{{this.summary.released}}</h2>
         <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{this.summary.name}}</h1>
         <div class="flex mb-4">
-          <span class="flex items-center">
+          <span class="flex items-center" v-for="i in this.summary.rating" :key="i">
             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
             </svg>
-            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <span class="text-gray-600 ml-3">4 Reviews</span>
+            
+            <!-- <span class="text-gray-600 ml-3">4 Reviews</span> -->
           </span>
           <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
             <a class="text-gray-500">
@@ -46,7 +35,7 @@
             </a>
           </span>
         </div>
-        <p class="leading-relaxed"></p>
+        <p v-html="this.summary.description" class="leading-relaxed"></p>
         <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
           <div class="flex">
             <span class="mr-3">Color</span>
