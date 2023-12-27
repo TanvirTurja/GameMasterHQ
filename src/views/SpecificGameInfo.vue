17de777ba0d74d57e077b20228bdf09a5aa0f043
Tanvir Turja <tanvirturja@yahoo.com>
2023-12-27 19:52:26 +0000

    test

--- src/views/SpecificGameInfo.vue
+++ src/views/SpecificGameInfo.vue
@@ -123,7 +123,7 @@ import axios from "axios";
            async getSummary(){
             try {
                 const response = await axios.get(`https://api.rawg.io/api/games/${this.id}?key=${this.apiKey}`)
-                this.summary = response.data
+                this.summary = response.data.results[0]
                 console.log(this.summary)
             } catch (error) {
                 console.log(error)
