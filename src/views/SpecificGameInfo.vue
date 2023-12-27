c36d65b3c10c5bf9f566ed0112c1b2c2580ee2bd
Tanvir Turja <tanvirturja@yahoo.com>
2023-12-27 20:19:53 +0000

    test

--- src/views/SpecificGameInfo.vue
+++ src/views/SpecificGameInfo.vue
@@ -1,13 +1,13 @@
 <template>
    
-      <p class="leading-relaxed text-lg">{{this.summary.name}}</p>
-     <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
+      <!-- <p class="leading-relaxed text-lg">{{this.summary.name}}</p>
+     <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span> -->
      <section class="text-gray-600 body-font overflow-hidden">
   <div class="container px-5 py-24 mx-auto">
     <div class="lg:w-4/5 mx-auto flex flex-wrap">
       <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400">
       <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
+        <h2 class="text-sm title-font text-gray-500 tracking-widest">{{this.summary.name}}</h2>
         <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
         <div class="flex mb-4">
           <span class="flex items-center">
