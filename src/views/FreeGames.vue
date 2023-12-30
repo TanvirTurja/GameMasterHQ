d6a489fde5628487fb3e409211a80b3909088750
Tanvir Turja <tanvirturja@yahoo.com>
2023-12-30 19:11:56 +0000

    button design done

--- src/views/FreeGames.vue
+++ src/views/FreeGames.vue
@@ -32,7 +32,7 @@
             <span v-else class="text-xs italic"> ...Click to expand </span>
           </p>
             <p class="mb-4 text-xs">{{i.instructions}}</p>
-           
+            <button @click="openLink(i.open_giveaway_url)" class="text-xs custom-btn btn-6">Go to Giveaway</button>
 
            
           </div>
@@ -92,13 +92,119 @@
 
       return description.slice(0, maxLength) + '...';
     },
-            
-         
-        },
-        
+   openLink(link) {
+      if (link) {
+        window.open(link, '_blank'); // Opens the link in a new tab
+      } else {
+        // Handle the case if there is no link available
+        console.log('No link available');
+      }
+    },
+        }
     }
 </script>
 
 <style scoped>
+
+.custom-btn {
+  width: 140px;
+  height: 40px;
+  color: #fff;
+  border-radius: 5px;
+  padding: 10px 25px;
+  font-family: 'Lato', sans-serif;
+  font-weight: 500;
+  background: transparent;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+   box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
+   7px 7px 20px 0px rgba(0,0,0,.1),
+   4px 4px 5px 0px rgba(0,0,0,.1);
+  outline: none;
+}
+
+
+/* 6 */
+.btn-6 {
+  background: rgb(100, 233, 122);
+background: radial-gradient(circle, rgb(100, 233, 122) 0%, rgb(193, 235, 43) 100%);
+  line-height: 42px;
+  padding: 0;
+  border: none;
+}
+.btn-6 span {
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.btn-6:before,
+.btn-6:after {
+  position: absolute;
+  content: "";
+  height: 0%;
+  width: 1px;
+ box-shadow:
+   -1px -1px 20px 0px rgba(255,255,255,1),
+   -4px -4px 5px 0px rgba(255,255,255,1),
+   7px 7px 20px 0px rgba(0,0,0,.4),
+   4px 4px 5px 0px rgba(0,0,0,.3);
+}
+.btn-6:before {
+  right: 0;
+  top: 0;
+  transition: all 500ms ease;
+}
+.btn-6:after {
+  left: 0;
+  bottom: 0;
+  transition: all 500ms ease;
+}
+.btn-6:hover{
+  background: transparent;
+  color: rgb(193, 235, 43);
+  box-shadow: none;
+}
+.btn-6:hover:before {
+  transition: all 500ms ease;
+  height: 100%;
+}
+.btn-6:hover:after {
+  transition: all 500ms ease;
+  height: 100%;
+}
+.btn-6 span:before,
+.btn-6 span:after {
+  position: absolute;
+  content: "";
+  box-shadow:
+   -1px -1px 20px 0px rgba(255,255,255,1),
+   -4px -4px 5px 0px rgba(255,255,255,1),
+   7px 7px 20px 0px rgba(0,0,0,.4),
+   4px 4px 5px 0px rgba(0,0,0,.3);
+}
+.btn-6 span:before {
+  left: 0;
+  top: 0;
+  width: 0%;
+  height: .5px;
+  transition: all 500ms ease;
+}
+.btn-6 span:after {
+  right: 0;
+  bottom: 0;
+  width: 0%;
+  height: .5px;
+  transition: all 500ms ease;
+}
+.btn-6 span:hover:before {
+  width: 100%;
+}
+.btn-6 span:hover:after {
+  width: 100%;
+}
+
 
 </style>
